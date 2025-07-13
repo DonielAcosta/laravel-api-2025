b86f6e6f6b9de5349d752dc306290fa613d75f94
Doniel Acosta <donielacosta1995@gmail.com>
2025-07-13 19:06:27 -0400

    creacion del login y ruta

--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,18 @@ use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\RecipeController;
 use App\Http\Controllers\Api\TagController;
 
+use App\Http\Controllers\Api\LoginController;
 
+Route::get('login',            [LoginController::class,'store']);
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->group(function(){
 
-Route::get('categories',            [CategoryController::class,'index']);
-Route::get('categories/{category}', [CategoryController::class,'show']);
-
-Route::apiResource('recipes', RecipeController::class);
-
-
-Route::get('tags',                  [TagController::class,'index']);
-Route::get('tags/{tag}',            [TagController::class,'show']);
+    Route::get('categories',            [CategoryController::class,'index']);
+    Route::get('categories/{category}', [CategoryController::class,'show']);
+    
+    Route::apiResource('recipes', RecipeController::class);
+    
+    
+    Route::get('tags',                  [TagController::class,'index']);
+    Route::get('tags/{tag}',            [TagController::class,'show']);
+});
